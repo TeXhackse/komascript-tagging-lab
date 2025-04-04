aa3dd74105414588f63a9cd224feb667d6a0bb31
Marei (peiTeX) <marei@peitex.de>
2025-04-04 17:19:59 +0200

    extend test update script to also update pdftexmain

--- tools/update_tests.sh
+++ tools/update_tests.sh
@@ -7,23 +7,24 @@
 shopt -s extglob
 
 for testdir in "testfiles" "testfiles-compare";
-do
+	do
 	testconfig=${testdir/!(*-*)?(-)}
 	echo "======================================="
 	echo "Updating $testdir ${testconfig:+using config $testconfig}"
 	echo "---------------------------------------"
-	for file in "$testdir"/*.lvt ;
-	 do
+	for file in "$testdir"/*.lvt ; do
 		file_base=${file##*/}
-	 	file_base=${file_base%%.*}
-		# testconfig is part after dash, empty if default config
-
-	 	if [ -f "$testdir/$file_base.luatex.tlg" ]; then
-			echo "Updating $file_base.luatex.tlg"
-	 		l3build save  ${testconfig:+-c "$testconfig"} -e luatex "$file_base" >> /dev/null
-	 	fi
+		file_base=${file_base%%.*}
 		echo "Updating $file_base.tlg"
 		l3build save ${testconfig:+-c "$testconfig"} "$file_base" >> /dev/null
+		# check for specific test configs
+		for engine in luatex pdftexmain; do
+			file_name="$file_base.$engine.tlg"
+			if [ -f "$testdir/$file_name" ]; then
+			echo "Updating $file_name"
+			l3build save  ${testconfig:+-c "$testconfig"} -e $engine "$file_base" >> /dev/null
+		fi
+		done
 	done
 	echo "---------------------------------------"
 done
