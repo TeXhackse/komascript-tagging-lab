2db16bfe9e455ffdb502cab57dc638ec19d42961
Marei (peiTeX) <marei@peitex.de>
2026-08-28 13:29:17 +0200

    add l3build doc to local pre commit config and fix typos

--- build.lua
+++ build.lua
@@ -1,36 +1,42 @@
 #!/usr/bin/env texlua
 svn_revision = 4281
-module          = "scr-tagging-lab"
+module = "scr-tagging-lab"
 
-typesetexe      = "lualatex-dev"
+typesetexe = "lualatex-dev"
 supportdir = "support-tests"
 
-docfiledir="experiments"
+docfiledir = "experiments"
 
 -- This also unpacks the KOMAscript files itself to simplify testing without a local copy of the dev status.
-sourcefiles     = { "*.dtx", "*.ins", "*.inc", "scrdocstrip.tex" }
-installfiles    = { "*.sty", "*.cls", "*.clo", "*.lco", "*.hak" }
-textfiles       = { "*.md", "*.txt", "README.md" }
-unpackfiles = {"scr-tagging-lab.ins","scrmain.ins"}
+sourcefiles = { "*.dtx", "*.ins", "*.inc", "scrdocstrip.tex" }
+installfiles = { "*.sty", "*.cls", "*.clo", "*.lco", "*.hak" }
+textfiles = { "*.md", "*.txt", "README.md" }
+unpackfiles = { "scr-tagging-lab.ins", "scrmain.ins" }
 
 -- This is only for demo files - does not include building the KOMAscript documentation
-typesetfiles={
-	"*.tex"
+typesetexe = "lualatex-dev"
+typesetopts = "--interaction=batchmode"
+
+typesetruns = 4
+typesetfiles = {
+	"*.tex",
+	"scrkernel-sections.dtx"
 }
+typesetopts = ""
 
 -- checkconfigs = {"build", "compare","tagging-project-tests"} -- compare config is only used when the real testfiles failed
 
-checksuppfiles={"*.tex","**/*.ltx"}
+checksuppfiles = { "*.tex", "**/*.ltx" }
 
 testsuppdir = "support-tests"
-typesetsuppfiles=checksuppfiles
-supportdir  = testsuppdir
+typesetsuppfiles = checksuppfiles
+supportdir = testsuppdir
 checkruns = 2
-typesetruns=2
-recordstatus=true
+typesetruns = 2
+recordstatus = true
 
-demofiles={"tagging-experiments/*.tex"}
-typesetdemofiles={"tagging-experiments/*.tex"}
+demofiles = { "tagging-experiments/*.tex" }
+typesetdemofiles = { "tagging-experiments/*.tex" }
 
 -- The following part of the configuration has been take from the tagpdf project by Ulrike Ficher
 -- Copyright (C) 2019-2024 Ulrike Fischer
@@ -39,17 +45,18 @@ typesetdemofiles={"tagging-experiments/*.tex"}
 
 -- tests are mostly with dev-format always
 specialformats = specialformats or {}
-specialformats["latex"] = specialformats["latex"] or
-  {
-    luatex     = {binary="luahbtex",format = "lualatex-dev"},
-    pdftex     = {format = "pdflatex-dev"},
-    pdftexmain = {binary="pdftex",format = "pdflatex"},
-    luatexmain = {binary="luahbtex",format = "lualatex"},
-  }
+specialformats["latex"] = specialformats["latex"]
+	or {
+		luatex = { binary = "luahbtex", format = "lualatex-dev" },
+		pdftex = { format = "pdflatex-dev" },
+		pdftexmain = { binary = "pdftex", format = "pdflatex" },
+		luatexmain = { binary = "luahbtex", format = "lualatex" },
+	}
 
 stdengine = luatex
-checkengines = {"luatex",
--- pdftex,
--- main track only for local tests,
---	"luatexmain","pdftexmain"
+checkengines = {
+	"luatex",
+	-- pdftex,
+	-- main track only for local tests,
+	--	"luatexmain","pdftexmain"
 }
